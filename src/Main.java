79e09d88e8a5457bde1d8020eb7a77091b28acc7
SergeyMarkelov <avemarksun@gmail.com>
2021-09-19 14:12:18 +0200

    =)

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
 public class Main {
+    public Main() {
+    }
 
     public static void main(String[] args) {
-        /* Przykłady wyświetlania tekstu w konsoli */
-        System.out.println("Witaj świecie!");
-        System.out.println("Po jest nasz pierwszy program w języku java");
-        System.out.println("Życzę" + " " + "wam " + "miłej nauki!");
+        String GREEN = "\u001b[0;32m";
+        System.out.println(GREEN + "Your pain is the breaking of the shell that encloses your understanding");
+        System.out.println("Even as the stone of the fruit must break, that its heart may stand in the sun, so must you know pain.");
+        System.out.println("And could you keep your heart in wonder at the daily miracles of your life your pain would not seem less ");
+        System.out.println("wondrous than your joy");
+        System.out.println("And you would accept the seasons of your heart, even as you have always accepted the seasons that pass ");
+        System.out.println("over your fields.");
+        System.out.println("And you would watch with serenity through the winters of your grief.");
+        System.out.println("Much of your pain is self-chosen.");
+        System.out.println("It is the bitter potion by which the physician within you heals your sick self.");
+        System.out.println("Therefore trust the physician, and drink his remedy in silence and tranquility:");
+        System.out.println("For his hand, though heavy and hard, is guided by the tender hand of the Unseen,");
+        System.out.println("And the cup he brings, though it burn your lips, has been fashioned of the clay which the Potter has");
+        System.out.println("moistened with His own sacred tears.");
 
-        System.out.printf("A %s zaczynamy z %d przykładem.\n", "więc", 1);
-
-        System.out.print("To jest jakiś");
-        System.out.print(" tekst");
-        System.out.print(" nie za długi.");
-
-//        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
-
+        /*
+        On Pain
+Kahlil Gibran - 1883-1931
+         */
     }
 }
