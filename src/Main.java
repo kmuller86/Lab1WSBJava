cab52c12b335e2543c766acf2c6b13297f3621df
SergeyMarkelov <avemarksun@gmail.com>
2021-09-21 18:46:01 +0200

    New changes maked by myself.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,45 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Witaj świecie!");
+        byte zmiennaTypuByte = 1;
+        short zmennaTypuShort = -2;
+        int zmiennaTypuInt = 3;
+        long zmiennaTypuLong = -100L;
+        float zmiennaTypuFloat = 100.1F;
+        double zmiennaTypuDouble = 132123.1231312;
+        boolean zmiennaTypuBoolean = true;
+        char zmeinnaTypuChar ='t';
+        String zmiennaTypuString = "Zmienna w jezyku Java";
+
+        System.out.println("zmiennaTypuByte = " + zmiennaTypuByte + " zmiennaTypuShort = " + zmennaTypuShort + " zmiennaTypuInt = " + zmiennaTypuInt);
+        System.out.println("zmiennaTypuLong = " + zmiennaTypuLong + " zmiennaTypuFloat = " + zmiennaTypuFloat + " zmiennaTypuDouble = " + zmiennaTypuDouble);
+        System.out.println("zmiennaTypuBoolean = " + zmiennaTypuBoolean + " zmeinnaTypuChar = " + zmeinnaTypuChar + " zmiennaTypuString = " + zmiennaTypuString);
+
+        // Zadanie 2 "Operacje na zmiennych";
+
+        int a = 1000;
+        int b = 333;
+        int zmiennaDodawania = a +b;
+        int zmiennaOdejmowania = a - b;
+        int zmiennaMnozenia = a * b;
+        int zmiennaDzielenia = a / b;
+        int zmiennaModulo = a % b;
+
+        System.out.println("1000+333= " + zmiennaDodawania);
+        System.out.println("1000-333= " + zmiennaOdejmowania);
+        System.out.println("1000*333= " + zmiennaMnozenia);
+        System.out.println("1000/333= " + zmiennaDzielenia);
+        System.out.println("1000%333= " + zmiennaModulo);
+
+
+
+
+
+
+
+
+
 
     }
+
 }
