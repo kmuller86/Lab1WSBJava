83b0a1855adc303f37c6d1d42e1fb7e27229305b
SergeyMarkelov <avemarksun@gmail.com>
2021-10-12 19:05:31 +0200

    Zrobiłem

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,12 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double[] tablicaDoCzwiczen = {9843.4, 131312.3, 3242423, 533453.76, 534233.567 };
+
+        for (int i = 0; i < tablicaDoCzwiczen.length; i++) {
+            System.out.println ("kontener numer " + i + " = " + tablicaDoCzwiczen[i]);
+        }
+
+
     }
 }
